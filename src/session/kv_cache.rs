f420ae6dd58898f46aa33a0662f10bdbdd80b442
Enoch Jung <enochjung514@gmail.com>
2026-06-02 14:57:11 +0900

    Add kv-cache

---
+++ src/session/kv_cache.rs
@@ -0,0 +1,9 @@
+pub(crate) struct KVCache {
+    // todo
+}
+
+impl KVCache {
+    pub(crate) fn new() -> Self {
+        Self {}
+    }
+}
--- src/session/mod.rs
+++ src/session/mod.rs
@@ -1,21 +1,31 @@
+mod kv_cache;
 mod special_token;
 
 use crate::Model;
+pub(crate) use kv_cache::KVCache;
+use std::marker::PhantomData;
 
-pub struct Session<'a> {
-    model: &'a Model<'a>,
-    tokens: Vec<u32>,
+mod private {
+    pub trait SessionState {}
 }
 
-impl<'a> Session<'a> {
-    pub fn new(model: &'a Model<'a>) -> Result<Self, crate::Error> {
-        let mut tokens = Vec::new();
-        tokens.push(special_token::BEGIN_OF_SENTENCE);
+pub struct Ready;
+impl private::SessionState for Ready {}
 
-        Ok(Self { model, tokens })
-    }
+pub struct Decoding;
+impl private::SessionState for Decoding {}
 
-    pub fn send_prompt(&mut self, user_input: &str) -> Result<String, crate::Error> {
+pub struct Session<'a, T: private::SessionState> {
+    model: &'a Model,
+    tokens: Vec<u32>,
+    kv_cache: KVCache,
+    _phantom: PhantomData<T>,
+}
+
+impl<'a> Session<'a, Ready> {
+    pub fn send_prompt(self, user_input: &str) -> Session<'a, Decoding> {
+        todo!()
+        /*
         self.tokens.push(special_token::USER);
 
         let mut input_tokens = self.model.tokenize(user_input)?;
@@ -25,93 +35,45 @@ impl<'a> Session<'a> {
         self.tokens.push(special_token::THINK_START);
 
         let embedded_tensor = self.model.build_embedding_vectors(&self.tokens)?;
-
-        todo!()
-
-        /*
-
-            let mut embedded_tensor = Tensor::<F32>::with_capacity(self.tokens.len() * 1536, 1536)?;
-
-            // word embedding
-            //// (model.embed_tokens.weight)
-            for token_id in self.tokens.iter() {
-                let tensor = self.embedding_engine.word_embed(*token_id)?;
-                embedded_tensor.append(&tensor)?;
-            }
-
-            // do
-            {
-                // for each layer [0, 28)
-                for layer_idx in 0..NUM_HIDDEN_LAYERS {
-                    // X
-
-                    // Attention(X: N*1536) -> N*1536
-                    {
-                        // input = X
-                        let mut attention_input = embedded_tensor.clone();
-
-                        // input rms norm
-                        //// (model.layers.#.input_layernorm.weight)
-                        self.normalization_engine
-                            .apply_input_rms_norm(layer_idx, &mut attention_input)?;
-
-                        // q k
-                        //// (model.layers.#.self_attn.q_proj.bias)
-                        //// (model.layers.#.self_attn.k_proj.bias)
-                        //// (model.layers.#.self_attn.q_proj.weight)
-                        //// (model.layers.#.self_attn.k_proj.weight)
-
-                        // rope(q, k)
-
-                        // v
-                        //// (model.layers.#.self_attn.v_proj.bias)
-                        //// (model.layers.#.self_attn.v_proj.weight)
-
-                        // concat header
-
-                        // output projection
-                        //// (model.layers.#.self_attn.o_proj.weight)
-                    }
-
-                    // residual (addition)
-                    // res := X + Attention(X)
-
-                    // FeedForward(X: N*1536) -> N*1536
-                    {
-                        // post rms norm
-                        //// (model.layers.#.post_attention_layernorm.weight)
-
-                        // input (N*1536)
-
-                        // gate : Wgate x input (N*8960)
-                        //// (model.layers.#.mlp.gate_proj.weight)
-                        // up   : Wup   x input (N*8960)
-                        //// (model.layers.#.mlp.up_proj.weight)
-
-                        // gate_silu : SiLU(gate)
-                        //// SiLU : x / (1 + e^(-x)) (element op)
-
-                        // up_proj : up * gate_silu (element-wise) (N*8960)
-
-                        // down_proj : Wdown x up_proj (N*1536)
-                        //// (model.layers.#.mlp.down_proj.weight)
-                    }
-
-                    // residual (addition)
-                    // res := X + FeedForward(X)
-                }
-
-                // rms norm (model.norm.weight)
-
-                // lm head (lm_head.weight)
-
-                // append embedding if not finished
-            }
-            // until eos
-
-            // return generated tokens with pretty format
-
-            todo!()
         */
+    }
+
+    pub(crate) fn new(model: &'a Model) -> Self {
+        let mut tokens = Vec::new();
+        tokens.push(special_token::BEGIN_OF_SENTENCE);
+        let kv_cache = KVCache::new();
+
+        Self {
+            model,
+            tokens,
+            kv_cache,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a> Session<'a, Decoding> {
+    pub fn start(&mut self) -> () {
+        todo!()
+    }
+
+    pub fn pause(&mut self) -> () {
+        todo!()
+    }
+
+    pub fn is_running(&self) -> bool {
+        todo!()
+    }
+
+    pub fn is_done(&self) -> bool {
+        todo!()
+    }
+
+    pub fn finish_decoding(self) -> Session<'a, Ready> {
+        todo!()
+    }
+
+    pub fn get_next_string(&mut self) -> Option<String> {
+        todo!()
     }
 }
